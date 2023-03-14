54b08014e48054c0b1290ed40e7c7af6a5accebc
Yeji <mnbvcxzyj@gmail.com>
2023-03-14 12:00:29 +0900

    initial_setting

---
+++ src/pages/fail.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const fail = () => {
+  return <div>실패</div>;
+};
+
+export default fail;
---
+++ src/pages/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const main = () => {
+  return <div>메인</div>;
+};
+
+export default main;
